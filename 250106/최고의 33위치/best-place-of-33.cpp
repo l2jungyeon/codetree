edab941a1b55140cc437318e097fc5d1d18e96d6
l2jungyeon <102033703+leejungyeon811@users.noreply.github.com>
2025-01-06 17:02:30 +0900

    Title: 최고의 33위치, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250106/최고의 33위치/best-place-of-33.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    //3*3 격자 내에
+
+    vector<vector<int>> vec(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> vec[i][j];
+        }
+    }
+
+    int large = 0;
+    for (int i = 0; i <= n-3; i++) {
+        for (int j = 0; j <= n-3; j++) {
+            int l = 0;
+            for (int dx = i; dx < i + 3; dx++) {
+                for (int dy = i; dy < i + 3; dy++) {
+                    l += vec[dx][dy];
+                }
+            }
+
+            if (l > large) large = l;
+        }
+    }
+
+    cout << large;
+}
