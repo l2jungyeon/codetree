d2dbaea6b2e99dd492024f3f035a91434b72d537
l2jungyeon <102033703+leejungyeon811@users.noreply.github.com>
2025-01-08 14:54:04 +0900

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250108/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 250108/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -5,15 +5,13 @@
 using namespace std;
 
 int solve(vector<vector<int>> vec, int x, int y, int k, int l) {
-	int dx = 0;
-	int dy = 0;
+	int dx; int dy;
 	int n = vec.size();//세로
 	int m = vec[0].size();//가로
 
 	for (int i = 0; i < k; i++) {
 		for (int j = 0; j < l; j++) {
-			if (x + i < n && x + i >= 0 && y + j < m && y+j >= 0
-				&& vec[x+i][y+j]>0) {
+			if (vec[x+i][y+j]==1) {
 				dx = i+1;
 				dy = j+1;
 			}
@@ -33,14 +31,16 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> vec[i][j];
+			if (vec[i][j] > 0) vec[i][j] = 1;
+			else vec[i][j] = 0;
 		}
 	}
 
 	int large = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			for (int k = 1; k <= n; k++) {	//가로길이
-				for (int l = 1; l <= m; l++) {//세로길이
+			for (int k = 1; k <= n-i; k++) {	//가로길이
+				for (int l = 1; l <= m-j; l++) {//세로길이
 					int res = solve(vec, i, j, k, l);
 					if (res > large) large = res;
 				}
