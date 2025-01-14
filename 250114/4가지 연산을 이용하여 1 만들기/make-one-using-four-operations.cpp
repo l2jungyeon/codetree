4f7a22b2d7ff78c96dd076205ce7dbb819905767
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-14 17:27:23 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 422ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 250114/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 250114/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -4,28 +4,29 @@
 using namespace std;
 
 int bfs(int n) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+	//priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+	queue<pair<int, int>> q;
 	//횟수, 현재 숫자
 	q.push({ 0,n });
 
 	while (!q.empty()) {
-		int cnt = q.top().first;
-		int num = q.top().second;
+		int cnt = q.front().first;
+		int num = q.front().second;
 		q.pop();
 
 		if (num == 1) return cnt;
 
 		for (int i = 0; i < 4; i++) {
-			if (i == 0) {
+			if (i == 3) {
 				q.push({ cnt + 1 , num+1 });
 			}
-			else if (i == 1) {
+			else if (i == 2) {
 				q.push({ cnt + 1 , num-1 });
 			}
-			else if (i == 2 && num % 2 == 0) {
+			else if (i == 1 && num % 2 == 0) {
 				q.push({ cnt + 1 , num / 2 });
 			}
-			else if (i == 3 && num % 3 == 0) {
+			else if (i == 0 && num % 3 == 0) {
 				q.push({ cnt + 1 , num / 3 });
 			}
 		}
