4fc4d441a180aff93a6d46b446bfef0d1a2e077a
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-14 14:26:22 +0900

    Title: 그래프 탐색, Time: 16ms, Memory: 4MB, Status: Passed - CodeTree

--- 250114/그래프 탐색/graph-traversal.cpp
+++ 250114/그래프 탐색/graph-traversal.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 using namespace std;
 
-void dfs(int x, int& cnt, vector<bool>& visited, vector<vector<int>> graph) {//1번정점에서 시작해서 도달할수있는 정점수
+void dfs(int x, int& cnt, vector<bool>& visited, const vector<vector<int>>& graph) {//1번정점에서 시작해서 도달할수있는 정점수
     int n = graph.size() - 1;
 
-    for (int child : graph[x]) {
-        if (!visited[child]) {
-            visited[child] = true;
-            dfs(child, ++cnt, visited, graph);
+    for (int i = 1; i < n + 1; i++) {
+        if (!visited[i] && graph[x][i]) {
+            visited[i] = true;
+            dfs(i, ++cnt, visited, graph);
         }
     }
 }
@@ -17,12 +17,12 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> graph(n + 1);
+    vector<vector<int>> graph(n + 1, vector<int>(n+1, false));
     for (int i = 0; i < m; i++) {
         int x, y;
         cin >> x >> y;
-        graph[x].push_back(y);
-        graph[y].push_back(x);//무방향
+        graph[x][y] = true;
+        graph[y][x] = true;
     }
 
     vector<bool>visited(n + 1, false);
