978eeebe112203d00e6aef7ca1a4c986327216a7
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-14 17:24:48 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 1034ms, Memory: 64MB, Status: Time Limit Exceed - CodeTree

--- 250114/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
+++ 250114/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -3,33 +3,30 @@
 #include <queue>
 using namespace std;
 
-int plusf(int a) { return a + 1; }
-int minusf(int a) { return a - 1; }
-int divideTwo(int a) { return a / 2; }
-int divideThree(int a) { return a / 3; }
-
 int bfs(int n) {
-	queue<pair<int,int>> q;//현재 숫자, 횟수
-	q.push({n,0});
-	
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+	//횟수, 현재 숫자
+	q.push({ 0,n });
+
 	while (!q.empty()) {
-		int num = q.front().first;
-		int cnt = q.front().second;
+		int cnt = q.top().first;
+		int num = q.top().second;
 		q.pop();
+
 		if (num == 1) return cnt;
 
 		for (int i = 0; i < 4; i++) {
 			if (i == 0) {
-				q.push({ plusf(num),cnt + 1 });
+				q.push({ cnt + 1 , num+1 });
 			}
 			else if (i == 1) {
-				q.push({ minusf(num),cnt + 1 });
+				q.push({ cnt + 1 , num-1 });
 			}
 			else if (i == 2 && num % 2 == 0) {
-				q.push({ divideTwo(num),cnt + 1 });
+				q.push({ cnt + 1 , num / 2 });
 			}
 			else if (i == 3 && num % 3 == 0) {
-				q.push({ divideThree(num),cnt + 1 });
+				q.push({ cnt + 1 , num / 3 });
 			}
 		}
 	}
