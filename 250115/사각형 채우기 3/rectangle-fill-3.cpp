17b5d9e7f6514146b90139b6f37b75b4c0dcf364
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-15 14:55:12 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250115/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250115/사각형 채우기 3/rectangle-fill-3.cpp
@@ -8,12 +8,18 @@ int main() {
     int n;
     cin>>n;
     vector<ll> dp(n+1);
+    dp[0]=1;
     dp[1]=2;
     dp[2]=7;
     for(int i=3;i<n+1;i++){
-        dp[i]=(dp[i-2]*4+dp[i-1]*2)%MOD;
+        dp[i]=(dp[i-2]*3)%MOD;
+        for(int j=0;j<i;j++){
+            if(j!=i-2) dp[i]+=(dp[j]*2)%MOD;
+        }
     }
     cout <<dp[n];
     return 0;
 }
-//dp[3]=dp[1]*4+dp[2]*2=8+14
+//dp[3]=dp[1]*4-1+dp[2]*2=8+14
+//22
+//dp[4]=dp[2]*4+dp[3]*
