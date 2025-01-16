124c1a2d3f48780f7e2bcadbcd2c42a78444046a
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-16 18:19:33 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250116/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
+++ 250116/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tuple>
 using namespace std;
 
 int main() {
@@ -12,31 +11,32 @@ int main() {
 		cin >> vec[i];
 	}
 
-	vector<tuple<int, int, int>> dp(n,{ 1, 1, 1 });
+	vector<int> updp(n,1);
+	vector<int> downdp(n,1);
+	vector<int> updowndp(n, 1);
+
 	for (int i = 1; i < n; i++) {
 		for (int j = 0; j < i; j++) {
 			if (vec[i] > vec[j]) {
-				get<0>(dp[i]) = max(get<0>(dp[i]), get<0>(dp[j]) + 1); //증가수열
+				updp[i] = max(updp[j] + 1, updp[i]);
 			}
 			else if (vec[i] < vec[j]) {
-				get<1>(dp[i]) = max(get<1>(dp[i]), get<1>(dp[j]) + 1); //감소수열
+				downdp[i] = max(downdp[j] + 1, downdp[i]);
 
-				if (get<2>(dp[j]) > 1) { //증가했다가 감소하는 수열이었을 때
-					get<2>(dp[i]) = max(get<2>(dp[j]) + 1, get<2>(dp[i]));
+				if (updowndp[j] > 1) { //증가했다가 감소하는 수열이었을 때
+					updowndp[i] = max(updowndp[j] + 1, updowndp[i]);
 				}
-				if (get<0>(dp[j]) > 1 && get<2>(dp[j]) == 1) {//증가했다가 감소하는 수열의 첫 감소 부분일 때
-					get<2>(dp[i]) = max(get<0>(dp[j]) + 1, get<2>(dp[i]));
+				else if (updp[j] > 1 && updowndp[j] == 1) {//증가했다가 감소하는 수열의 첫 감소 부분일 때
+					updowndp[i] = max(updp[j] + 1, updowndp[i]);
 				}
 			}
 		}
 	}
 
-
 	int large=0;
 	for (int i = 0; i < n; i++) {
-		if (large < get<0>(dp[i])) large = get<0>(dp[i]);
-		if (large < get<1>(dp[i])) large = get<1>(dp[i]);
-		if (large < get<2>(dp[i])) large = get<2>(dp[i]);
+		int l = max({ updp[i],downdp[i],updowndp[i] });
+		if (large < l) large = l;
 	}
 
 	cout << large;
