33988088390e0129959b1e9575e5c554af979fed
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-16 17:15:02 +0900

    Title: 2차원 최대 증가 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250116/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 250116/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
+#include <tuple>
 #include <climits>
 using namespace std;
 
@@ -10,30 +10,33 @@ int main() {
 	cin >> n >> m;
 
 	vector<vector<int>> vec(n, vector<int>(m));
+	vector<tuple<int, int, int>> cell;	//value,x,y
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> vec[i][j];
+			cell.push_back({ vec[i][j],i,j });
 		}
 	}
+	sort(cell.begin(), cell.end());	//오름차순으로 정렬(작은것부터 처리)
 
 	vector<vector<int>> dp(n, vector<int>(m, INT_MIN));
 	//i,j까지 왔을 때 밟을 수 있는 최대 칸 수
 	dp[0][0] = 1;
-	queue<pair<int, int>> q;
-	q.push({ 0,0 });
+	
+	for (tuple<int, int, int> t : cell) {
+		int x = get<1>(t);
+		int y = get<2>(t);
+		int v = get<0>(t);
 
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
+		for (tuple<int, int, int> preT : cell) {
+			int prex = get<1>(preT);
+			int prey = get<2>(preT);
+			int preV = get<0>(preT);
 
-		for (int i = x+1; i < n; i++) {
-			for (int j = y + 1; j < m; j++) {
-				if (vec[x][y] < vec[i][j]) {
-					dp[i][j] = max(dp[i][j], dp[x][y] + 1);
-					q.push({ i,j });
-				}
+			if (x > prex && y > prey && preV < v) {
+				dp[x][y] = max(dp[x][y], dp[prex][prey] + 1);
 			}
+
 		}
 	}
 
@@ -44,5 +47,5 @@ int main() {
 		}
 	}
 
-    cout <<large;
+	cout << large;
 }
