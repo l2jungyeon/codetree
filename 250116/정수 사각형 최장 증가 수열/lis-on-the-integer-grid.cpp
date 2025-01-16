c208d0c51eca084e83d6bdb07b59875e35d35917
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-16 14:59:00 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1055ms, Memory: 46MB, Status: Time Limit Exceed - CodeTree

--- 250116/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 250116/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -8,6 +8,10 @@ const int dx[] = { 0,0,1,-1 };
 const int dy[] = { 1,-1,0,0 };
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
 	int n;
 	cin >> n;
 
@@ -32,9 +36,11 @@ int main() {
 		int cury = q.front().second;
 		q.pop();
 
+
 		for (int d = 0; d < 4; d++) {
 			int x = curx + dx[d];
 			int y = cury + dy[d];
+
 			if (x < n && x >= 0 && y < n && y >= 0){
 				if (vec[x][y] > vec[curx][cury]) {//다음께 더 큰 경우
 					dp[x][y] = dp[curx][cury] + 1;
@@ -44,9 +50,17 @@ int main() {
 					dp[curx][cury] = max(dp[x][y] + 1,dp[curx][cury]);
 				}
 			}
-			
 		}
 	}
+
+	/*cout << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}*/
+
 
 	int large = 0;
 	for (int i = 0; i < n; i++) {
