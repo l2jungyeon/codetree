20fae4530278610b90a458946f6b21cd34d855f9
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-16 14:46:34 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1020ms, Memory: 40MB, Status: Time Limit Exceed - CodeTree

---
+++ 250116/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+const int dx[] = { 0,0,1,-1 };
+const int dy[] = { 1,-1,0,0 };
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<vector<int>> vec(n, vector<int>(n));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> vec[i][j];
+		}
+	}
+
+	vector<vector<int>> dp(n, vector<int>(n,1));
+	//인접한 칸이 자신보다 작으면 인접한칸dp +1
+	queue<pair<int, int>> q;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			q.push({ i,j });
+		}
+	}
+
+	while (!q.empty()) {
+		int curx = q.front().first;
+		int cury = q.front().second;
+		q.pop();
+
+		for (int d = 0; d < 4; d++) {
+			int x = curx + dx[d];
+			int y = cury + dy[d];
+			if (x < n && x >= 0 && y < n && y >= 0){
+				if (vec[x][y] > vec[curx][cury]) {//다음께 더 큰 경우
+					dp[x][y] = dp[curx][cury] + 1;
+					q.push({ x,y });
+				}
+				else if (vec[x][y] < vec[curx][cury]) {//다음께 더 작은 경우
+					dp[curx][cury] = max(dp[x][y] + 1,dp[curx][cury]);
+				}
+			}
+			
+		}
+	}
+
+	int large = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (large < dp[i][j]) { large = dp[i][j]; }
+		}
+	}
+	cout << large;
+
+}
