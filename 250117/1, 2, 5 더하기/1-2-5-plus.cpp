4d293fed4bd33dbb84c488f2e2cc0aef44ba05a5
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-17 17:47:27 +0900

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250117/1, 2, 5 더하기/1-2-5-plus.cpp
+++ 250117/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -15,7 +15,7 @@ int main() {
 	for (int i = 1; i < n + 1; i++) {
 		for (int j = 0; j < 3; j++) {
 			if(i-vec[j]>=0)
-				dp[i] += dp[i - vec[j]] + 1;
+				dp[i] =(dp[i]+ dp[i - vec[j]] + 1)%MOD;
 		}
 	}
 
