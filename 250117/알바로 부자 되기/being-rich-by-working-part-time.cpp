cef1ef71737a0c4504781f738d468e288a4ee680
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-17 16:38:53 +0900

    Title: 알바로 부자 되기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250117/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> vec(n+1);
+	vector<int> cost(n+1);
+
+	for (int i = 1; i <= n; i++) {
+		int a, b;
+		cin >> a >> b >> cost[i];
+		vec[i] = { a,b };
+	}
+	
+	vector<int> dp(n+1);
+	dp[0] = 0;
+	dp[1] = cost[1];
+	
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (vec[j].second >= vec[i].first) {//겹치면
+				dp[i] = max({ dp[j - 1] + cost[i], dp[i], dp[j]});
+			}
+			else dp[i] = max(dp[j] + cost[i],dp[i]);
+		}
+	}
+
+	/*for (int i = 1; i <= n; i++) {
+		cout << dp[i]<<endl;
+	}*/
+	cout << dp[n];
+}
