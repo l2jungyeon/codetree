f34fe768e3008675858df8e2361238753b74c562
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-17 17:45:38 +0900

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250117/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	//n을 1,2,5의 합으로 나타내는 방법의 수
+	vector<int> vec = { 1,2,5 };
+	vector<int> dp(n + 1,0);
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 0; j < 3; j++) {
+			if(i-vec[j]>=0)
+				dp[i] += dp[i - vec[j]] + 1;
+		}
+	}
+
+	cout << dp[n] - dp[n - 1];
+}
