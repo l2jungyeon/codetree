2cafea706d9baedfef441892e947f94f5f3c2d96
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-17 16:46:45 +0900

    Title: 알바로 부자 되기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250117/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 250117/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -7,30 +7,31 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<pair<int, int>> vec(n+1);
-	vector<int> cost(n+1);
+	vector<pair<int, int>> vec(n);
+	vector<int> cost(n);
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		int a, b;
 		cin >> a >> b >> cost[i];
 		vec[i] = { a,b };
 	}
-	
-	vector<int> dp(n+1);
-	dp[0] = 0;
-	dp[1] = cost[1];
-	
-	for (int i = 2; i <= n; i++) {
-		for (int j = 1; j < i; j++) {
+
+	vector<int> dp(n);
+	for (int i = 0; i < n; i++) {
+		dp[i] = cost[i];
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
 			if (vec[j].second < vec[i].first) {//안겹치면
-				dp[i] = max({ dp[j] + cost[i], dp[i]});
+				dp[i] = max({ dp[j] + cost[i], dp[i] });
 			}
 		}
 	}
 
-	/*for (int i = 1; i <= n; i++) {
-		cout << dp[i]<<endl;
-	}*/
-	sort(dp.begin(),dp.end());
-	cout << dp[dp.size()-1];
+	// for (int i = 0; i < n; i++) {
+	// 	cout << dp[i]<<endl;
+	// }
+	sort(dp.begin(), dp.end());
+	cout << dp[n - 1];
 }
