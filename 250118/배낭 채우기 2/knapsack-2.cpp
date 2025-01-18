1c91c12c02bab7e92727a72958d6d6a5c17f95db
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-18 17:02:33 +0900

    Title: 배낭 채우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250118/배낭 채우기 2/knapsack-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	//배낭문제.
+	//근데 중복으로 선택이 가능한...
+
+	vector<vector<int>> vec(n, vector<int>(2)); //0->무게, 1->가치
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i][0] >> vec[i][1];
+	}
+
+	vector<int> dp(m + 1,0); //idx 무게일 때의 최대 가치
+	for (int i = 0; i < n; i++) { //각 보석에서
+		for (int j = 0; j < m+1; j++) {//각 무게당
+			if (j >= vec[i][0])
+				dp[j] = max(dp[j - vec[i][0]] + vec[i][1], dp[j]);
+		}
+	}
+
+	/*for (int i = 0; i < m + 1; i++) {
+		cout << dp[i] << endl;
+	}*/
+
+	cout << dp[m];
+}
