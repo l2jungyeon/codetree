8f844ae67134f898e049107fc7daa3ded1e9cb20
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-18 21:14:16 +0900

    Title: 계단 오르기 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250118/계단 오르기 2/climbing-stairs-2.cpp
+++ 250118/계단 오르기 2/climbing-stairs-2.cpp
@@ -10,30 +10,43 @@ int main() {
 
 	int n;
 	cin >> n;
-	
+
 	vector<int>vec(n);
 	for (int i = 0; i < n; i++) {
 		cin >> vec[i];
 	}
 
+	if (n <= 2) {
+		cout << vec[0] + vec[1];
+		return 0;
+	}
+
 	//dp[i][j]=1계단을 i개 올랐고, j번째 계단까지 고려했을 때의 최대합
 	//dp[i][j] = max(dp[i-1][j-1]+vec[i], dp[i][j-1],dp[i][j])
 	//j번째 계단을 오르는 경우(한계단 오르기) or j번째 계단을 오르지 않는 경우(두계단 오르기)
+	//j-1번째 계단에서 2계단을 오르는 경우
 	vector<vector<int>> dp(4, vector<int>(n));
 	dp[0][0] = 0;
 	dp[0][1] = vec[1];
 	dp[1][0] = vec[0];
+	dp[2][1] = vec[0] + vec[1];
+	dp[3][2] = vec[0] + vec[1] + vec[2];
 
 	for (int i = 2; i < n; i++) { //1계단을 0개 올랐고, i번째 계단까지 고려
 		if (i % 2 == 0) dp[0][i] = 0;
 		else dp[0][i] = dp[0][i - 2] + vec[i];
 	}
 
-	for (int i = 1; i < 4; i++) {
-		for (int j = 1; j < n; j++) {
-			dp[i][j] = max({ dp[i - 1][j - 1] + vec[j],dp[i][j - 1],dp[i][j] });
+	for (int i = 1; i < 4; i++) { //1계단을 i개 올랐고, j번째 계단까지 고려
+		for (int j = i; j < n; j++) {
+			if (j == n - 1) dp[i][j] = max({ dp[i][j], dp[i][j - 2] + vec[j],dp[i - 1][j - 1] + vec[j] });
+			else {
+				dp[i][j] = max({ dp[i - 1][j - 1] + vec[j],dp[i][j - 1] ,dp[i][j] });
+				if (j - 2 >= 0)dp[i][j] = max(dp[i][j], dp[i][j - 2] + vec[j]);
+			}
+
 		}
 	}
 
-	cout << dp[3][n-1];
+	cout << dp[3][n - 1];
 }
