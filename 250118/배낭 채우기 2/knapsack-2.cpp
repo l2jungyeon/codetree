0cf6d36b36c6efe073a9b7f706889f533d4468b9
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-18 17:03:47 +0900

    Title: 배낭 채우기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250118/배낭 채우기 2/knapsack-2.cpp
+++ 250118/배낭 채우기 2/knapsack-2.cpp
@@ -17,8 +17,8 @@ int main() {
 	}
 
 	vector<int> dp(m + 1,0); //idx 무게일 때의 최대 가치
-	for (int i = 0; i < n; i++) { //각 보석에서
-		for (int j = 0; j < m+1; j++) {//각 무게당
+	for (int j = 0; j < m+1; j++) { //각 무게당
+		for (int i = 0; i < n; i++) {//각 보석마다
 			if (j >= vec[i][0])
 				dp[j] = max(dp[j - vec[i][0]] + vec[i][1], dp[j]);
 		}
