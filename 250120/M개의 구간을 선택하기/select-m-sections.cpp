4e9071bcbc2f5e8cb5518f70ba48040d2f9641d9
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-20 17:35:34 +0900

    Title: M개의 구간을 선택하기, Time: 1024ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 250120/M개의 구간을 선택하기/select-m-sections.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+	}
+
+	int ans = INT_MIN;
+
+	for (int startIdx = 0; startIdx < n; startIdx++) {
+		//idx 0부터인것...
+		vector<vector<ll>> dp(m + 1, vector<ll>(n, INT_MIN));
+		dp[1][startIdx] = vec[startIdx];
+		for (int i = startIdx+1; i < n; i++) dp[1][i] = dp[1][i - 1] + vec[i];
+
+		for (int i = 2; i < m + 1; i++) {
+			for (int j = (i - 1) * 2 + startIdx; j < n; j++) {
+				if (j == (i - 1) * 2 + startIdx) {
+					dp[i][j] = dp[i - 1][j - 2] + vec[j];
+					continue;
+				}
+				for (int maxJ = (i - 1) * 2 + startIdx-1; maxJ < j-1; maxJ++){
+					dp[i][j] = max({ dp[i - 1][maxJ] + vec[j], dp[i][j - 1] + vec[j],dp[i][j] });
+				}
+			}
+		}
+
+		/*if(startIdx == 0 || startIdx ==1){
+		for (int i = 1; i < m + 1; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << dp[i][j] << " ";
+			}
+			cout << endl;
+		}
+		}*/
+
+		for (int j = 0; j < n; j++) {
+			if (ans < dp[m][j]) ans = dp[m][j];
+		}
+	}
+	
+	cout << ans;
+}
