67bfe6da8a404569b95599bafbdc0ee9b3705a6a
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-20 18:03:58 +0900

    Title: M개의 구간을 선택하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250120/M개의 구간을 선택하기/select-m-sections.cpp
+++ 250120/M개의 구간을 선택하기/select-m-sections.cpp
@@ -6,50 +6,43 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n, m;
-	cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
 
-	vector<int> vec(n);
-	for (int i = 0; i < n; i++) {
-		cin >> vec[i];
-	}
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
 
-	int ans = INT_MIN;
+    int ans = INT_MIN;
 
-	for (int startIdx = 0; startIdx <= n-(2*m-1); startIdx++) {
-		//idx 0부터인것...
-		vector<vector<ll>> dp(m + 1, vector<ll>(n, INT_MIN));
-		dp[1][startIdx] = vec[startIdx];
-		for (int i = startIdx+1; i < n; i++) dp[1][i] = dp[1][i - 1] + vec[i];
+    for (int startIdx = 0; startIdx <= n - (2 * m - 1); startIdx++) {
+        vector<vector<ll>> dp(m + 1, vector<ll>(n, INT_MIN));
+        dp[1][startIdx] = vec[startIdx];
 
-		for (int i = 2; i < m + 1; i++) {
-			for (int j = (i - 1) * 2 + startIdx; j < n; j++) {
-				if (j == (i - 1) * 2 + startIdx) {
-					dp[i][j] = dp[i - 1][j - 2] + vec[j];
-					continue;
-				}
-				for (int maxJ = (i - 1) * 2 + startIdx-2; maxJ < j-1; maxJ++){
-					dp[i][j] = max({ dp[i - 1][maxJ] + vec[j], dp[i][j - 1] + vec[j],dp[i][j] });
-				}
-			}
-		}
-		
-			// for (int i = 1; i < m + 1; i++) {
-			// 	for (int j = 0; j < n; j++) {
-			// 		cout << dp[i][j] << " ";
-			// 	}
-			// 	cout << endl;
-			// }
-			// cout << endl;
-		
-		for (int j = 0; j < n; j++) {
+        // Precompute max values for dp[i-1][j-2]
+        vector<ll> maxPrev(n, INT_MIN);
+        maxPrev[startIdx] = vec[startIdx];
+        for (int i = startIdx + 1; i < n; i++) {
+            maxPrev[i] = max(maxPrev[i - 1], dp[1][i]);
+        }
+
+        for (int i = 2; i <= m; i++) {
+            vector<ll> maxDp(n, INT_MIN);
+            for (int j = (i - 1) * 2 + startIdx; j < n; j++) {
+                if (j >= 2) {
+                    dp[i][j] = maxPrev[j - 2] + vec[j];
+                }
+                maxDp[j] = max(maxDp[j - 1], dp[i][j]);
+            }
+            maxPrev = maxDp;
+        }
+
+        for (int j = 0; j < n; j++) {
 			if (ans < dp[m][j]) ans = dp[m][j];
-		}
-	}
-	
-	cout << ans;
-}
+        }
+    }
 
-/*10 3
--300 -200 -600 -231 -997 -456 -10 -2 -3 -1
-*/
+    cout << ans;
+    return 0;
+}
