3511d2380b95f599eaf9bd49f969917745c2ed21
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-01-21 17:23:49 +0900

    Title: 수정 수집하기, Time: 7ms, Memory: 1MB, Status: Passed - CodeTree

--- 250121/수정 수집하기/collect-crystals.cpp
+++ 250121/수정 수집하기/collect-crystals.cpp
@@ -6,42 +6,63 @@
 using namespace std;
 
 int main() {
-	int n, k;
-	cin >> n >> k;
+	int n, l;
+	cin >> n >> l;
 	
 	vector<char> vec(n);
 	for (int i = 0; i < n; i++) {
 		cin >> vec[i];
 	}
+	//dp[k][i][j] j번째 수정, 현재 i번 움직임, 첫번째가 k(L,R)인경우
+	vector<vector<vector<int>>> dp(2,vector<vector<int>>(l+1, vector<int>(n)));
 
-	//dp[i][j] = j번째 이전까지 i(0-k)번 움직였고 j번째 수정을 고려하고 있을 때
-	vector<vector<int>> dp(k+1, vector<int>(n));
+	int ans = 0;
 
-	int p = 1;
-	for (int i = 0; i < n; i++) {
-		if (vec[i] == 'L'){
-			dp[0][i] += p;
-			p++;
-		}
-	}
-	dp[1][0] = 0;
+	for (int k = 0; k < 2; k++) {
+		//k==0: 첫시작이 L
+		//k==1: 첫시작이 R
 
-	for (int i = 1; i < k+1; i++) {
-		char preLR = i % 2 == 1 ? 'R' : 'L';
-		for (int j = i; j < n; j++) {
-			//이전 인덱스와 위치가 같다면 그대로+1
-			if (preLR == vec[j]) {
-				dp[i][j] = dp[i][j - 1] + 1;
+		int p = 1;
+		for (int i = 0; i < n; i++) {
+			if (vec[i] == (k==0?'L':'R')) {
+				if (i == 0 && vec[i] == 'R' && k == 1) continue;
+				dp[k][0][i] += p;
+				p++;
 			}
-			else {//움직인 후의 위치와 다르다면
-				//j번째에서 움직이기 vs 안움직이기
-				dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i][j - 1]);
-				if (dp[i][j] == dp[i - 1][j - 1] + 1) {
-					preLR = vec[j];
+		}
+
+		for (int i = 1; i < l + 1; i++) {
+			char preLR;
+			if (k==0) preLR = i % 2 == 1 ? 'R' : 'L'; //움직인 후의 위치를 가정
+			else preLR = i % 2 == 1 ? 'L' : 'R';
+
+			for (int j = i; j < n; j++) {
+				//이전 인덱스와 위치가 같다면 그대로+1
+				if (preLR == vec[j]) {
+					dp[k][i][j] = dp[k][i][j - 1] + 1;
+				}
+				else {//움직인 후의 위치와 다르다면
+					//j번째에서 움직이기 vs 안움직이기
+
+					if (j == i) {
+						dp[k][i][j] = dp[k][i - 1][j - 1] + 1; //첫시작이라면 무조건 움직여주기
+						preLR = vec[j];
+						continue;
+					}
+
+					dp[k][i][j] = max(dp[k][i - 1][j - 1] + 1, dp[k][i][j - 1]);
+					if (dp[k][i][j] == dp[k][i - 1][j - 1] + 1) {
+						preLR = vec[j];
+					}
 				}
 			}
 		}
+		
+		for (int i = 0; i < l + 1; i++) {
+			ans = max(ans, dp[k][i][n - 1]);
+		}
 	}
+	
+	cout << ans;
 
-	cout << dp[k][n-1];
 }
