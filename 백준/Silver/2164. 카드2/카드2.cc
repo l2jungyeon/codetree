79e8b2d0be5c213376823188922e288bd7f4d4f9
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-02-03 15:50:45 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		q.push(i + 1);
+	}
+
+	while (q.size() != 1) {
+		q.pop();
+		int x = q.front();
+		q.pop();
+		q.push(x);
+	}
+
+	cout << q.front();
+}
