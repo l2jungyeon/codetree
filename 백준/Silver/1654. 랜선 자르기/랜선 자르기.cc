77ff9216c88eeb5c9f56dbb4baaa91693c6ea9ed
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-02-21 20:44:49 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,43 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
+#include <climits>
+
 typedef long long ll;
-
-vector<ll> vec;
-
-//start부터 end까지의 값을 binary search
-ll solve(int n) {
-	ll start = 1, end = vec[vec.size() - 1];
-	ll mid = (start + end) / 2;
-
-	while (start <= end) {
-		ll cnt = 0;
-		for (ll len : vec) cnt += len / mid;
-
-		if (cnt < n) { //좌측 탐색
-			end = mid - 1;
-		}
-		else { //우측탐색
-			start = mid + 1;
-		}
-		mid = (start + end) / 2;
-
-	}
-	
-	return mid;
-}
+using namespace std;
 
 int main() {
-	int k, n;
-	cin >> k >> n;
+    int k, n;
+    cin >> k >> n;
+    vector<ll> vec(k);
+    for (int i = 0; i < k; i++) cin >> vec[i];
+    sort(vec.rbegin(), vec.rend());
 
-	for (int i = 0; i < k; i++) {
-		ll a;
-		cin >> a;
-		vec.push_back(a);
-	}
-	sort(vec.begin(), vec.end());
 
-	cout << solve(n);
+    //최대 랜선의 길이
+    //이분탐색!!~~!!
+    ll start = 1;
+    ll end = vec[0];
+    ll ans = 0;
+    while (start <= end) {
+        ll middle = (start + end) / 2;
+        ll cnt = 0;
+        for (int i = 0; i < k; i++) {
+            cnt += (vec[i] / middle);
+        }
+
+        if (cnt < n) end = middle-1;
+        else if (cnt >= n) {
+            ans = middle;
+            start = middle+1;
+        }
+    }
+
+    cout << ans;
 }
