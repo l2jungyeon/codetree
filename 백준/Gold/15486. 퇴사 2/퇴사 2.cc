d09ae933653528ab3c463600087e750356d54d33
l2jungyeon <102033703+l2jungyeon@users.noreply.github.com>
2025-02-26 18:24:45 +0900

    [Gold V] Title: 퇴사 2, Time: 312 ms, Memory: 95892 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    vector<vector<ll>> vec(n, vector<ll>(2)); //vec[i][0]: 날짜, 1: 이익
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i][0] >> vec[i][1];
+    }
+
+    vector<ll> dp(n + 1, 0);
+    for (int i = n - 1; i >= 0; i--) {
+        ll day = vec[i][0];
+        ll coin = vec[i][1];
+
+        if (i + day > n) {
+            dp[i] = dp[i + 1];
+            continue;
+        }
+
+        dp[i] = max(dp[i + day] + coin, dp[i+1]);
+    }
+
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, dp[i]);
+    }
+    cout << ans;
+}
